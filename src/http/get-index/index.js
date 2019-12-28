52f16616080af4fb1000fa94fe5ed5208cb6eca8
Mike Brevoort <mbrevoort@slack-corp.com>
2019-12-28 10:50:50 -0700

    Simple example with data

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,41 +1,34 @@
 // Add simple, fast, scalable persistence: https://docs.begin.com/en/data/begin-data/
-// let data = require('@begin/data')
+let data = require('@begin/data')
+let begin = require('@architect/functions') // Reads & writes session data
 
 // Add secure sessions, middleware, and more: https://docs.begin.com/en/functions/http/
 // let arc = require('@architect/functions')
 
 // TODO: modify the body object!
-let body = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
-
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
-    </h1>
-
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
-
-  </body>
-</html>
+let body = `{
+  "hello": "world",
+  "number": 1
+}
 `
 
 exports.handler = async function http(req) {
+  let table = 'global'
+  let key = 'request_count'
+  let count = 0
+  let doc = await data.get({table, key})
+  if (doc && doc.count) {
+    count = doc.count
+  }
+  count++
+  await data.set({table, key, count})
+
   return {
+    statusCode: 200,
     headers: {
-      'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+      'content-type': 'application/json'
     },
-    body
+    body: JSON.stringify({ count })
   }
 }
 
