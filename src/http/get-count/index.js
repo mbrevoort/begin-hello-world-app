cea9ca78b9b802672826451cc181eb46e55425ff
Mike Brevoort <mbrevoort@slack-corp.com>
2019-12-28 11:28:10 -0700

    Copy get-index to get-count

--- src/http/get-count/index.js
+++ src/http/get-count/index.js
@@ -1,38 +1,21 @@
-// Enable secure sessions, express-style middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
+let data = require('@begin/data')
 
-let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
-  </head>
-  <body>
-    <h1 class="center-text">
-      Hello world!
-    </h1>
-    <p class="center-text">
-      Your new route is ready to go!
-    </p>
-    <p class="center-text">
-      Learn more about building <a href="https://docs.begin.com/en/functions/http/" class="link" target="_blank">Begin HTTP functions here</a>.
-    </p>
-  </body>
-</html>
-`
-
-// HTTP function
 exports.handler = async function http(req) {
-  console.log(req)
+  let table = 'global'
+  let key = 'request_count'
+  let count = 0
+  let doc = await data.get({table, key})
+  if (doc && doc.count) {
+    count = doc.count
+  }
+  count++
+  await data.set({table, key, count})
+
   return {
+    statusCode: 200,
     headers: {
-      'content-type': 'text/html; charset=utf8'
+      'content-type': 'application/json'
     },
-    body: html
+    body: JSON.stringify({ count })
   }
 }
